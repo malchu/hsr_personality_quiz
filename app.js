94b8710eb748b23e642993b4fa8bf5ed23f727a9
malchu <malchupascual@gmail.com>
2024-07-10 20:32:24 -0400

    slideshow

--- app.js
+++ app.js
@@ -3,6 +3,32 @@ import characters from './characters.js'
 
 document.addEventListener('DOMContentLoaded', function() 
   {
+    const images = [
+      "./assets/mini.gif",
+      "./assets/mini1.gif",
+      "./assets/loading.gif",
+    ];
+
+    let currentImageIndex = 0;
+    const slideshowImage = document.getElementById('background');
+
+    function showNextSlide() {
+      // Increment currentImageIndex and wrap around
+      currentImageIndex++;
+      if (currentImageIndex >= images.length) {
+        currentImageIndex = 0;
+      }
+
+      // Update the src attribute of the image element
+      slideshowImage.src = images[currentImageIndex];
+
+      // Change slide every 3 seconds (3000 milliseconds)
+      setTimeout(showNextSlide, 3000);
+    }
+
+    // Call showNextSlide to start the slideshow
+    showNextSlide();
+    
     const button = document.getElementById("start-button");
     button.addEventListener("click", function() 
       {
@@ -12,27 +38,5 @@ document.addEventListener('DOMContentLoaded', function()
         questionPanel.style.display = "flex";
       }
     );
-
-    let questionIndex = 0;
-
-    const nextButton = document.getElementById("next-button");
-    nextButton.addEventListener("click", function() 
-      {
-        nextQuestion(questionIndex);
-      }
-    );
-
-    function nextQuestion(index) {
-      const currentQuestion = questions[questionIndex];
-      const questionContainer = document.getElementById("question-container");
-
-      questionContainer.innerHTML = '';
-
-      if (index < questions.length) {
-
-      } else {
-        calculateAndDisplayResults(responses);
-      }
-    }
   }
 );
